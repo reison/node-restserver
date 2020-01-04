5e941ad75cdbb3f9639533ecc577de908df50a7a
Reison Ruiz <reisonruizcastro@gmail.com>
2020-01-04 02:05:11 -0300

    otra vez la misma clave 2

--- server/config/config.js
+++ server/config/config.js
@@ -42,5 +42,5 @@ process.env.URLDB = urlDB;
 // google cliente id
 //======================================
 
-process.env.CLIENT_ID = process.env.CLIENT_ID || '1061092881054-27sqrf165nk38too8u461qoeie0ktsao.apps.googleusercontent.com';
-//process.env.CLIENT_ID = process.env.CLIENT_ID || '1061092881054-2d8pkvhfjhs7vvs8embtllroge6p3s4v.apps.googleusercontent.com';
+//process.env.CLIENT_ID = process.env.CLIENT_ID || '1061092881054-27sqrf165nk38too8u461qoeie0ktsao.apps.googleusercontent.com';
+process.env.CLIENT_ID = process.env.CLIENT_ID || '1061092881054-2d8pkvhfjhs7vvs8embtllroge6p3s4v.apps.googleusercontent.com';
